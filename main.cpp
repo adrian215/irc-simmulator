a2c94cb76dc767c81cde36d3f7f117a2a28c92cb
necia425 <necia425@gmail.com>
2015-05-12 21:03:13 +0200

    config

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "config/AppConfig.h"
+#include <stdio.h>
 
 using namespace std;
 
-int main() {
+int main(int argc,char *argv[]) {
+
+    AppConfig * appConfig = new AppConfig();
     cout << "Hello, World!" << endl;
     return 0;
 }
